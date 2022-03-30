2141ddca1979b2b3c610974de4eecf415acf3b0b
rafaelrspatrocinio <rafael.silvapatrocinio@everis.nttdata.com>
2022-03-30 14:54:25 -0300

    Metodo Get da classe users

---
+++ src/main/java/com/br/apirest/domain/User.java
@@ -0,0 +1,32 @@
+package com.br.apirest.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
