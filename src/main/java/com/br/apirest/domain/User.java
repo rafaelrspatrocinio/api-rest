5de917eadb5ed16c0bfdd50799ed05e9ac773c70
rafaelrspatrocinio <rafael.silvapatrocinio@everis.nttdata.com>
2022-04-04 14:47:35 -0300

    Referenciando os posts do usuário

--- src/main/java/com/br/apirest/domain/User.java
+++ src/main/java/com/br/apirest/domain/User.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "user")
 public class User implements Serializable {
@@ -21,6 +23,15 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
     @Override
     public boolean equals(Object o) {
